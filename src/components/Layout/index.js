72e9d64d77917b5616d83047b7fa9ab1f2619c50
Kedzi The Eagle <kedzitheeagle@Kedzis-Mac-mini.local>
2022-10-11 21:23:29 +0200

    styles and bootstrap

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import "styles/app.scss"
+
 const Layout = ({ children }) => {
   return <main>{children}</main>
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
 
+import Layout from "../components/Layout"
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <div className="container">
+        <div className="row">
+        <div className="col-6">Hello world!</div>
+        <div className="col-6">Hello world!</div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
