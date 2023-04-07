64e7a4c99c85a0003f2bc79f14209fa90bbdbae0
Bartek Kedziora <kedzitheeagle@Kedzis-Mac-mini.local>
2023-04-07 10:31:46 +0200

    services refactor

--- src/page_components/single-service/ServiceContent/index.js
+++ src/page_components/single-service/ServiceContent/index.js
@@ -5,54 +5,58 @@ import Button from "components/Button"
 import Divider from "components/Divider"
 
 const ServiceContent = ({ data }) => {
-  console.log(data)
-
   return (
     <section className="single-service">
-      <div className="container-fluid">
-        {data?.acfServices?.row?.map(
-          (
-            {
-              blockImage,
-              blockTitle,
-              blockSubtitle,
-              blockText,
-              button,
-              buttonLink,
-            },
-            index
-          ) => {
-            const odd = index % 2 === 1
-            return (
-              <div
-                key={index}
-                className={`single-service__row${odd ? " odd" : ""}`}
-              >
-                <div className="single-service__img">
-                  <img src={blockImage?.sourceUrl} alt={blockImage?.altText} />
-                </div>
-                <div className="single-service__content">
-                  <p className="single-service__title">{blockTitle}</p>
-                  {blockSubtitle && (
-                    <p className="single-service__subtitle">{blockSubtitle}</p>
-                  )}
-                  <div
-                    className="single-service__text"
-                    dangerouslySetInnerHTML={{ __html: blockText }}
-                  />
-                  {button ? (
-                    <Button to={buttonLink} className="button-dark">
-                      {button}
-                    </Button>
-                  ) : (
-                    <Divider mt={20} />
-                  )}
+      {data?.acfServices?.row?.map(
+        (
+          {
+            blockImage,
+            blockTitle,
+            blockSubtitle,
+            blockText,
+            button,
+            buttonLink,
+          },
+          index
+        ) => {
+          const odd = index % 2 === 1
+          return (
+            <div key={index} className="single-service__row--padding">
+              <div key={index} className="single-service__row--wrapper">
+                <div className="container">
+                  <div className={`single-service__row${odd ? " odd" : ""}`}>
+                    <div className="single-service__img">
+                      <img
+                        src={blockImage?.sourceUrl}
+                        alt={blockImage?.altText}
+                      />
+                    </div>
+                    <div className="single-service__content">
+                      <p className="single-service__title">{blockTitle}</p>
+                      {blockSubtitle && (
+                        <p className="single-service__subtitle">
+                          {blockSubtitle}
+                        </p>
+                      )}
+                      <div
+                        className="single-service__text"
+                        dangerouslySetInnerHTML={{ __html: blockText }}
+                      />
+                      {button ? (
+                        <Button to={buttonLink} className="button-dark">
+                          {button}
+                        </Button>
+                      ) : (
+                        <Divider mt={20} />
+                      )}
+                    </div>
+                  </div>
                 </div>
               </div>
-            )
-          }
-        )}
-      </div>
+            </div>
+          )
+        }
+      )}
     </section>
   )
 }
